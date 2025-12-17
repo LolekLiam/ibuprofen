55f01f7c9367aed7411bc3b3959d7beb07b58244
LolekLiam <63475764+LolekLiam@users.noreply.github.com>
2025-12-17 22:36:01 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "v0.8.0-alpha"
+        versionName = "v0.9.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
